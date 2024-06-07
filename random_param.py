e990a6a084384c7583a11ec860a9b62af721b034
idergunoff <idergunoff@yandex.ru>
2024-06-07 11:30:29 +0300

    random param

--- random_param.py
+++ random_param.py
@@ -232,7 +232,7 @@ def push_random_param():
         n_sig_bot = random.randint(ui_rp.spinBox_bot_skip_up.value(), ui_rp.spinBox_bot_skip_down.value())
 
         if n_sig_bot + n_sig_top > 505:
-            n_sig_bot = 505 - n_sig_top
+            n_sig_bot = random.randint(0, 505 - n_sig_top)
 
         def get_n(ts: str) -> str:
             if ts == 'distr':
