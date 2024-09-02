bc343466bce6b976370d914753f125e15a393e8a
idergunoff <idergunoff@yandex.ru>
2024-09-02 11:20:41 +0300

    regressor_form

--- regression.py
+++ regression.py
@@ -510,12 +510,17 @@ def train_regression_model():
     """ Расчет регрессионной модели """
     data_train, list_param_name = build_table_train(True, 'regmod')
     list_param_reg = get_list_param_numerical_for_train(list_param_name)
-    list_nan_param, count_nan = [], 0
+    list_nan_param, count_nan = set(), 0
     for i in data_train.index:
         for param in list_param_reg:
             if pd.isna(data_train[param][i]):
                 count_nan += 1
-                list_nan_param.append(param)
+                list_nan_param.add(param)
+            if data_train[param][i] == np.inf or data_train[param][i] == -np.inf:
+                data_train[param][i] = 0
+                count_nan += 1
+                list_nan_param.add(param)
+
     if count_nan > 0:
         list_col = data_train.columns.tolist()
         data_train = pd.DataFrame(imputer.fit_transform(data_train), columns=list_col)
