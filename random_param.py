8fd9f9d6a8037909b5303f917a395a33c4645ed7
idergunoff <idergunoff@yandex.ru>
2024-09-12 17:14:37 +0300

    rnd param new list param

--- random_param.py
+++ random_param.py
@@ -684,7 +684,7 @@ def push_random_param():
                 if param.startswith('sig'):
                     p, atr, up, down = param.split('_')[0], param.split('_')[1], int(param.split('_')[2]), 512 - int(param.split('_')[3])
                     for i_sig in range(up, down):
-                        new_list_param.append(f'sig_{atr}_{i_sig}')
+                        new_list_param.append(f'sig_{atr}_{i_sig + 1}')
                 else:
                     p, atr, n = param.split('_')[0], param.split('_')[1], int(param.split('_')[2])
                     for i_sig in range(n):
