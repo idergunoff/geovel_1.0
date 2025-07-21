5957d67c04f57e0600c84cab1e9df38a03ea8112
DAGubajdullina <dinara.gubaidullina@gmail.com>
2025-07-21 09:40:12 +0300

    build_table_train() - except JSONDecodeError

--- build_table.py
+++ build_table.py
@@ -29,10 +29,12 @@ def build_table_train(db=False, analisis='lda'):
         if data[0]:
             try:
                 data_train = pd.read_parquet(data[0])
+                return data_train, list_param
             except OSError:
-                data_train = pd.DataFrame(json.loads(data[0]))
-
-            return data_train, list_param
+                try:
+                    return pd.DataFrame(json.loads(data[0])), list_param
+                except JSONDecodeError:
+                    pass
 
     data_train, _ = build_table_train_no_db(analisis, analisis_id, list_param)
     return data_train, list_param
