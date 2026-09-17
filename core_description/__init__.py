a4aa6bc455c6e81f4379fb9096e0bdc8bd0148bb
idergunoff <57505310+idergunoff@users.noreply.github.com>
2026-09-17 15:25:36 +0300

    Implement batch core description import

--- core_description/__init__.py
+++ core_description/__init__.py
@@ -2,10 +2,15 @@
 
 from .models import ParsedCoreDocument, ParsedCoreInterval, RockMention, SemanticMatch
 from .parser import CoreDescriptionParseError, parse_core_document
+from .batch import BatchParseItem, BatchParseResult, discover_documents, parse_batch
 from .semantics import DictionaryValidationError, analyze_description, load_dictionary
 
 __all__ = [
     "CoreDescriptionParseError",
+    "BatchParseItem",
+    "BatchParseResult",
+    "discover_documents",
+    "parse_batch",
     "ParsedCoreDocument",
     "ParsedCoreInterval",
     "RockMention",
